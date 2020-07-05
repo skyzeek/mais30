9a45fa5985553e1c44784f81be854a4adc3a9808
sky <61230757+skyzeek@users.noreply.github.com>
2020-07-05 11:52:02 +0900

    Update mais.js

--- mais.js
+++ mais.js
@@ -2,17 +2,16 @@
 $(function(){
   // 1回目のアクセス
   if($.cookie("access") == undefined) {
-    //alert("初回です");
+    alert("このアプリはmais30です\nまずこのページは”録音”です。\nその隣がクイズ、説明、概要となっています。\n説明は、楽譜の読み方やリコーダーの指などです。\n概要は、このアプリのことが書いてあるので是非見てください")
     $.cookie("access","onece");
     $(".mod_message").css("display","block")
   // 2回目以降
   } else {
-    //alert("二回目以降です");
+    alert("二回目以降のご利用ありがとうございます");
   }
 });
  
 // Message close
 $(function() {
   $(".mod_message .close").click(function(){
-    $(".mod_message").css("display","none")
   });
